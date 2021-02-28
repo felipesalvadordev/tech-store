b82d07ee52f8db2d3611a70e266fdc9ace75ae90
felipe-salvador-fitcard <f.salvador91@gmail.com>
2021-02-28 17:47:30 -0300

    config. injeção de dependência contexto de vendas

---
+++ src/TechStore.WebApp/Setup/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechStore.Core.Communication.Mediator;
+using MediatR;
+using TechStore.Core.Messages.Notifications;
+using TechStore.Vendas.Application.Queries;
+using TechStore.Venda.Domain;
+using TechStore.Vendas.Data;
+using TechStore.Vendas.Application.Commands;
+
+namespace TechStore.WebApp.Setup
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+            services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
+            services.AddScoped<IPedidosQueries, PedidosQueries>();
+            services.AddScoped<VendasContext>();
+
+            services.AddScoped<IRequestHandler<IniciarPedidoCommand, bool>, PedidoCommandHandler>();
+        }
+    }
+}
--- src/TechStore.WebApp/Startup.cs
+++ src/TechStore.WebApp/Startup.cs
@@ -6,10 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TechStore.Vendas.Data;
+using MediatR;
+using TechStore.WebApp.Setup;
 
 namespace TechStore.WebApp
 {
@@ -26,6 +30,13 @@ namespace TechStore.WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<VendasContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddMediatR(typeof(Startup));
+
+            services.RegisterServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
