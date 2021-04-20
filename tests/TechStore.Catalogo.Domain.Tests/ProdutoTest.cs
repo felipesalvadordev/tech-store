92fda1d2f4bcf311311ecbd726b42a826786eae3
felipe-salvador-fitcard <f.salvador91@gmail.com>
2021-04-20 00:03:27 -0300

    testes validações produto

--- tests/TechStore.Catalogo.Domain.Tests/ProdutoTest.cs
+++ tests/TechStore.Catalogo.Domain.Tests/ProdutoTest.cs
@@ -7,13 +7,18 @@ namespace TechStore.Catalogo.Domain.Tests
     public class ProdutoTest
     {
         [Fact]
-        public void TesteProdutoSemNome()
+        public void TesteValidacoesProduto()
         {
             var ex = Assert.Throws<DomainException>(() =>
                new Produto(string.Empty, "Descricao", false, 100, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
            );
 
             Assert.Equal("O campo Nome do produto não pode estar vazio", ex.Message);
+
+            ex = Assert.Throws<DomainException>(() => 
+            new Produto("Nome", string.Empty, false, 100, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(1, 1, 1)));
+
+            Assert.Equal("O campo Descricao do produto não pode estar vazio", ex.Message);
         }
     }
 }
